8331772f79351104258093124b1679e321d02a9c
kurtdariel <kurtdariel@gmail.com>
2020-05-18 23:49:15 +0800

    Section 10: FINISHED

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,33 +1,14 @@
-// setTimeout( () => {
-//     console.log('Two seconds are up!')
-// }, 2000)
-
-// const names = ['Kurt', 'Alyssa']
-// const shortNames = names.filter((name) => {
-//     return name.length <= 4
-// })
-
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitude: 0,
-//             longitude: 0
-//         }
-    
-//         callback(data)
-//     }, 2000)
-// }
-
-// geocode('philadelphia', (data) => {
-//     console.log(data)
-// })
-
-const add = (firstNum, secondNum, callback) => {
-    setTimeout(() => {
-        callback (firstNum + secondNum)
-    }, 2000)
+const doWorkCallback = (callback) => {
+ setTimeout(() => {
+    // callback('This is my error', undefined)
+    callback(undefined, [1, 4, 7])
+ }, 2000)
 }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    console.log(result)
 })
