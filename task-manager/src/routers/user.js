647052fb08ee51a3f8ad31469699c544a59d030d
kurtdariel <kurtdariel@gmail.com>
2020-05-25 22:28:23 +0800

    Section 15: FINISHED

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancelEmail } = require('../emails/account')
 const router = new express.Router()
 const multer = require('multer')
 
@@ -10,6 +11,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -82,6 +84,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendCancelEmail(req.user.email, req.user.name)  
         res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
